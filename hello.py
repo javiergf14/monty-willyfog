2680d4cd4c34fa9c5db58212f063849bda629871
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-02-20 10:10:00 +0100

    Debuggin

--- hello.py
+++ hello.py
@@ -44,6 +44,7 @@ def moneda2_page():
     codigo_pais = select_where(cursor, 'Id', 'Nombre', pais, 'TBL_PAIS')
 
     monedas = select_monedas2(cursor, codigo_pais)
+
     return render_template('13moneda_page.html', **locals())
 
 
@@ -55,6 +56,10 @@ def pagadora_page2():
     id_monedas = select_where(cursor, 'Id', 'Nombre', monedas, 'TBL_MONEDA')
 
     pagadoras = select_pagadoras2(cursor, codigo_pais, id_monedas, grupos_pagador)
+
+    sqlquery = "select Empresa FROM TBL_PAGADOR WHERE Id_pais = {} AND Id_Moneda = {} " \
+               "AND Id_GrupoPagador = {}".format(codigo_pais, id_monedas, grupos_pagador)
+    print(sqlquery)
 
     return render_template('14pagadora_page.html', **locals())
 
