52fa4853a478ba2d16cacc02b79a20b886c3bca8
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-02-28 12:46:44 +0100

    Omit blank lines within the document.

--- src/montydb.py
+++ src/montydb.py
@@ -52,7 +52,8 @@ def transform_to_template(raw_doc, raw_header, format_array):
     formatted_doc = []
     formatted_doc.append(raw_header)
     for cont, raw_row in enumerate(raw_doc):
-        if cont < offset: continue 
+        if cont < offset: continue
+        if raw_row == [';']*len(raw_header): continue ## blank line
         row = ['']*len(raw_header)
         for key, value in format_array.items():
             row[raw_header.index(key)] = str(raw_row[value-1])       
