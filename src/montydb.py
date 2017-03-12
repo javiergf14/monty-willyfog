c1e7cba96cf816b01d5b29184b433292cfa949e2
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-03-12 19:25:34 +0100

    Debug

--- src/montydb.py
+++ src/montydb.py
@@ -172,7 +172,7 @@ def insert_rows(rows, table_name,  cursor, raw_header, db_header, format_array, 
     """
 
     if "TipoPago" not in format_array:
-        raw_header = raw_header.pop("TipoPago")
+        raw_header.pop("TipoPago", None)
     for elem in rows:
         # Constructing a row with the database format in order to check if the row does already exist.
         condition = ''
